ad62fe44f9695fc0060f3bdeaa76f019b24bf8bd
esHack <eswaryaga@gmail.com>
2016-12-11 21:56:42 +0530

    Update ProcessServlet.java

--- src/main/java/servlet/ProcessServlet.java
+++ src/main/java/servlet/ProcessServlet.java
@@ -25,7 +25,7 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import com.nc.processors.DataProcessorImpl;
 
 @WebServlet("/Process")
-public class ProcessServlet extends HttpServlet {
+public class Process extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private ServletFileUpload uploader = null;
 
